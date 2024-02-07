a2b51b49006e2529b303c9a8e9186d16276d703e
baynibea <158351681+baynibea@users.noreply.github.com>
2024-02-07 17:34:29 -0400

    Add files via upload

---
+++ Projectbea.java
@@ -0,0 +1,21 @@
+
+package projectbea;
+import javax.swing.JOptionPane;
+
+public class Projectbea {
+   
+    
+    public static void main(String[] args) {
+        String segundavia = 
+                JOptionPane.showInputDialog(" deseja fazer a segunda via do seu cartão?");
+      
+        If (segundavia = "sim"){
+        JOptionPane.showInputDialog("ok, você recebera a segunda via do seu cartão em breve");
+        
+    }else{
+            JOptionPane.showInputDialog("você não recebera a segunda via do cartão:)");
+            }
+    
+    }
+    
+}
